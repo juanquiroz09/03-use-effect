58a1a2aab7b7c558026532ae33a8824520f52d10
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-11-25 16:51:23 -0500

    use-effect

--- src/components/PruebasComponents.js
+++ src/components/PruebasComponents.js
@@ -1,17 +1,31 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const PruebasComponents = () => {
 const [user, setUser] = useState("Juan");
+const [date, setDate] = useState("01-01-1998");
 const modUser = (e) => {
     setUser(e.target.value);
 }
+const changeDate = e => {
+  setDate(Date.now());
+}
+//Solo se ejecuta una vez
+useEffect(()=>{
+  console.log("has cargado el componente")
+},[]);
+//Se ejecuta solo si cambio usuario
+useEffect(()=>{
+  console.log("has modificado el usuario")
+},[user]);
   return (
     <div>
         <h1>UseEffect</h1>
         <strong className='label label-green'>{user}</strong>
-        <form>
+        <strong>{date}</strong>
+        <p>
           <input type="text" onChange={modUser} placeholder="Cambia el Nombre"/>
-        </form>
+        </p>
+        <button onClick={changeDate}>Cambiar Fecha</button>
 
     </div>
   )
